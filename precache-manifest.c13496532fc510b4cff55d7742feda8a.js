edb38c288c741f8c0e945149b47ebf66a40ab77c
Hiran Roy <hironsrrijan@gmail.com>
2022-02-26 22:50:57 +0600

    Updates

---
+++ precache-manifest.c13496532fc510b4cff55d7742feda8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52fc8d4c269ab1f1fd5beb0fd436ef65",
+    "url": "/chater/index.html"
+  },
+  {
+    "revision": "6fb66df176368dc7fade",
+    "url": "/chater/static/css/main.4c4d18bd.chunk.css"
+  },
+  {
+    "revision": "45bc0318b4c82da31812",
+    "url": "/chater/static/js/2.97103c1f.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/chater/static/js/2.97103c1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fb66df176368dc7fade",
+    "url": "/chater/static/js/main.7a84ff37.chunk.js"
+  },
+  {
+    "revision": "98720397de3b660ed6fd",
+    "url": "/chater/static/js/runtime-main.d327b837.js"
+  }
+]);
